bf0eaaf57071fc18b946a735bf4a43efc6868a43
Bruno Silveira | Stone <bruno.silveira@stone.com.br>
2019-11-06 15:44:25 -0300

    Script to drop the tables

---
+++ Scripts/Drop_Tables.sql
@@ -0,0 +1,12 @@
+DROP TABLE [Subscription];
+DROP TABLE [Class];
+DROP TABLE [Course];
+DROP TABLE [InformationArea];
+DROP TABLE [TeacherProfile];
+DROP TABLE [Student];
+DROP TABLE [Teacher];
+DROP TABLE [Level];
+DROP TABLE [Profile];
+/*
+	Como deixar isso automático sem precisar colocar os nomes das tabelas?
+*/
